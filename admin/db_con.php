2e1a1df22cb80f62d58ec6da19e9488792d868fc
ricardo-monreal <54251540+ricardo-monreal@users.noreply.github.com>
2021-11-28 08:27:58 -0700

    Update db_con.php

--- admin/db_con.php
+++ admin/db_con.php
@@ -1,7 +1,7 @@
 <?php 
 define('DBHOST', 'yjo6uubt3u5c16az.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
 define('DBUSER', 'kxhgn1xo5fturqnv');
-define('DBNAME', 'student');
+define('DBNAME', 'expx9nh3wqtjtsu4');
 define('DBPASSWORD', 'clkwwkfl6wuayto6');
 
 
