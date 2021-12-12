b6ca56745754676dd4e195a40efcbdab64eb50bc
ricardo-monreal <54251540+ricardo-monreal@users.noreply.github.com>
2021-12-12 10:55:01 -0700

    Update db_con.php

--- admin/db_con.php
+++ admin/db_con.php
@@ -5,4 +5,4 @@ define('DBNAME', 'student');
 define('DBPASSWORD', 'gcpTest1DB');
 
 
-$db_con = mysqli_connect(DBHOST, DBUSER, DBPASSWORD, DBNAME);
+$db_con = mysqli_connect(null, DBUSER, DBPASSWORD, DBNAME, null, "/cloudsql/cst-323-gcp:us-west4:cst-323-gcp-db");
